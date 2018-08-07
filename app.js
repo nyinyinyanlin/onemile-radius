6780473bfa96d688dc1f5b78a5aa08b90ceae8c5
nyinyinyanlin <nyinyinyanlin.mm@gmail.com>
2018-08-07 17:31:12 +0630

    Set process.env.PORT

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+var port = process.env.PORT || 8000
 
 app.use('/html2canvas',express.static(path.join(__dirname,'node_modules/html2canvas/dist/')));
+app.use('/canvas2image/canvas2image.js',express.static(path.join(__dirname,'static/canvas2image.js')))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'static/map.html')))
-
-app.listen(3000, () => console.log("Example app listening to port 3000!"))
+app.listen(port, () => console.log("Example app listening to port:"+port))
